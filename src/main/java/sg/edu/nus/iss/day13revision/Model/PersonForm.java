6be66336186983ba59ba194b1ae61158139e2cc4
delanyeong <delan.april@gmail.com>
2022-07-21 10:14:59 +0800

    created person and personform models

---
+++ src/main/java/sg/edu/nus/iss/day13revision/Model/PersonForm.java
@@ -0,0 +1,40 @@
+package sg.edu.nus.iss.day13revision.Model;
+
+public class PersonForm {
+    private String firstName;
+    private String lastName;
+
+
+    public PersonForm() {
+    }
+
+    public PersonForm(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " firstName='" + getFirstName() + "'" +
+            ", lastName='" + getLastName() + "'" +
+            "}";
+    }
+
+}
